3f4a5300468246ed1d1643b087ede6a388719cbe
TJ <dhyani@naturallink.ai>
2024-12-08 19:01:31 -0600

    fix font imports in next.js

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from 'next/font/google';
+import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter',
+  subsets: ["latin"],
+  variable: "--font-inter",
 });
 
 export const metadata: Metadata = {
